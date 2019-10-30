58c33a132d1ae1310178c3eb7a475a56a89724b7
Joely Vernier <55214971+jorovernier@users.noreply.github.com>
2019-10-30 15:06:10 -0700

    Finished all steps

---
+++ src/ducks/userReducer.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const initialState = {
+    email: null,
+    firstName: null,
+    lastName: null
+}
+
+const REQUEST_USER_DATA = 'REQUEST_USER_DATA';
+
+export function requestUserData(){
+    let data = axios.get('/auth/user-data').then(response => response.data)
+    return {
+        type: REQUEST_USER_DATA,
+        payload: data
+    }
+}
+
+export default function userReducer(state = initialState, action){
+    const {type, payload} = action;
+    switch (type) {
+        case REQUEST_USER_DATA + '_FULFILLED':
+            const {email, firstName, lastName} = payload.user
+            return {email, firstName, lastName};
+        default:
+            return state;
+    }
+}
